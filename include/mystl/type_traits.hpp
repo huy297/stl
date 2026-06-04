9e9d957f03784d579efe72f163c756a7ed9f46ab
huy297 <duchuy2972005@gmail.com>
2026-06-04 18:42:15 +0700

    type_traits

---
+++ include/mystl/type_traits.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+namespace mystl {
+// integral_constant
+template <typename T, T v>
+struct integral_constant {
+    static constexpr T value = v;
+
+    using value_type = T;
+    using type = integral_constant;
+
+    constexpr operator value_type() const noexcept {
+        return value;
+    }
+
+    constexpr value_type operator()() const noexcept {
+        return value;
+    }
+};
+
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
+
+// is same
+template <typename A, typename B>
+struct is_same : false_type {};
+
+template <typename A>
+struct is_same<A, A> : true_type {};
+
+
+template <typename A, typename B>
+inline constexpr bool is_same_v = is_same<A, B>::value;
+
+// remove reference
+template <typename T>
+struct remove_reference {
+    using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&> {
+    using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&&> {
+    using type = T;
+};
+
+template <typename T>
+using remove_reference_t = typename remove_reference<T>::type;
+
+// remove const
+
+template <typename T>
+struct remove_const {
+    using type = T;
+};
+
+template <typename T>
+struct remove_const<const T> {
+    using type = T;
+};
+
+template <typename T>
+using remove_const_t = typename remove_const<T>::type;
+
+// remove volatile
+
+template <typename T>
+struct remove_volatile {
+    using type = T;
+};
+
+template <typename T>
+struct remove_volatile<volatile T> {
+    using type = T;
+};
+
+template <typename T>
+using remove_volatile_t = typename remove_volatile<T>::type;
+
+// remove cv
+template <typename T>
+struct remove_cv {
+    using type = remove_volatile_t<remove_const_t<T>>;
+};
+
+template <typename T>
+using remove_cv_t = typename remove_cv<T>::type;
+
+
+template <typename T>
+struct remove_cvref {
+    using type = remove_cv_t<remove_reference_t<T>>;
+};
+
+template <typename T>
+using remove_cvref_t = typename remove_cvref<T>::type;
+
+}
